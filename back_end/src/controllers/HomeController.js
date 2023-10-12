d71221e850c864c8b7bd65fcae1e3bc6104aea0f
Tran Thai Duy <ttduycapricorn@gmail.com>
2023-10-13 00:07:34 +0700

    2

---
+++ back_end/src/controllers/HomeController.js
@@ -0,0 +1,12 @@
+ const getHomePage = (req, res) => {
+    return res.render('HomePage.ejs')
+}
+
+ const getAboutPage = (req, res) => {
+    return res.render('test/aboutPage.ejs')
+}
+
+module.exports ={
+    getHomePage: getHomePage,
+    getAboutPage:getAboutPage
+}
--- back_end/src/route/web.js
+++ back_end/src/route/web.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
+import HomeController from '../controllers/HomeController';
+
 const router = express.Router('/');
 
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('hello world with Tran Thai Duy');
-    });
+    router.get('/', HomeController.getHomePage);
+
+    router.get('/about', HomeController.getAboutPage);
+
+    // rest AIP
 
     return app.use('/', router);
 };
